bce0f276dd9f9547c23460cb80e16125d52d04a8
reena <sreenarani45@gmail.com>
2021-06-02 16:15:17 -0500

    branching by creating develop

--- src/SelJava/proj2.java
+++ src/SelJava/proj2.java
@@ -6,6 +6,7 @@ public class proj2 {
 		// TODO Auto-generated method stub
 System.out.println("my first project2");
 System.out.println("i made changes in second commit");
+System.out.println("i made changes in third commit");
 	}
 	
 
