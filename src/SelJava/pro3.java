4608168b5de1c2eeca9982db734fca64f74fe92a
reena <sreenarani45@gmail.com>
2021-06-02 16:35:06 -0500

    proj3 commits

---
+++ src/SelJava/pro3.java
@@ -0,0 +1,12 @@
+package SelJava;
+
+public class pro3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first project3");
+		System.out.println("i made changes in proj3 commit");
+		System.out.println("i made changes in proj3 sec commit");
+	}
+
+}
