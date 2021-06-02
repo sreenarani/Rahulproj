848748d0e0845e87ae16a75fb4446818aa0739b1
reena <sreenarani45@gmail.com>
2021-06-02 15:46:23 -0500

    modified proj2

--- src/SelJava/proj2.java
+++ src/SelJava/proj2.java
@@ -5,6 +5,7 @@ public class proj2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("my first project2");
+System.out.println("i made changes in second commit");
 	}
 	
 
